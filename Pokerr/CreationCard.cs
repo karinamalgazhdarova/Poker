1547e70a760426b52ec4c714e87dd71a2f8a374d
karinamalgazhdarova <malgazhdarova.karina@gmail.com>
2019-09-18 22:56:26 +0600

    Добавьте файлы проекта.

---
+++ Pokerr/CreationCard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokerr
+{
+    public class CreationCard
+    {
+        string[] pack = {"туз черви","туз буби","туз крести","туз пики",
+                         "король черви", "король буби", "король крести", "король пики",
+                         "валет черви", "валет буби", "валет крести", "валет пики",
+                         "дама черви", "дама буби", "дама крести", "дама пики",
+                         "10 черви", "10 буби", "10 крести", "10 пики",
+                         "9 черви", "9 буби", "9 крести", "9 пики",
+                         "8 черви", "8 буби", "8 крести", "8 пики",
+                         "7 черви", "7 буби", "7 крести", "7 пики",
+                         "6 черви", "6 буби", "6 крести", "6 пики",
+                         "5 черви", "5 буби", "5 крести", "5 пики",
+                         "4 черви", "4 буби", "4 крести", "4 пики",
+                         "3 черви", "3 буби", "3 крести", "3 пики",
+                         "2 черви", "2 буби", "2 крести", "2 пики", };
+
+        string[] mixedPack = new string[52];
+
+        public void mixPack()
+        {
+            Random r = new Random();
+            for (int i = 0; i < pack.Length - 1; i++)
+            {
+
+                while (true)
+                {
+                    int k = r.Next(0, 52);
+
+                    if (mixedPack[k] == null)
+                    {
+                        mixedPack[k] = pack[i];
+                        break;
+                    }
+
+                }
+
+
+            }
+        }
+        public string[] getPack()
+        {
+            return mixedPack;
+        }
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Pokerr/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokerr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите количество игроков:");
+            while (true)
+
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+
+                if (n < 2 || n > 10)
+                {
+                    Console.WriteLine("Введите число игроков от 2 до 10");
+                }
+                else
+                {
+                    break;
+
+                }
+            }
+            CreationCard creationCard = new CreationCard();
+            creationCard.mixPack();
+            for (int i = 0; i < n * 5; i++)
+            {
+                if (0 == i % 5)
+                {
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(creationCard.getPack()[i]);
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
